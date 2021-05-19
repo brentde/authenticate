ef974842cde13a0b895da3596e951b0d8b7e92a2
Brent Deaver <bmdeaver18@gmail.com>
2021-05-18 17:35:52 -0700

    Resolved routing module bug

--- authentication/src/app/app-routing.module.ts
+++ authentication/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '/', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent},
   { path: 'home', component: HomeComponent }
 ];
 
