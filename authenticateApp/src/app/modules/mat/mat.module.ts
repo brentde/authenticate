2e5ef37934a0f8c5af32954288f0f5086b7f3247
Brent Deaver <bmdeaver18@gmail.com>
2021-05-20 10:07:08 -0700

    Basic login screen

--- authenticateApp/src/app/modules/mat/mat.module.ts
+++ authenticateApp/src/app/modules/mat/mat.module.ts
@@ -1,9 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import {  NgModule } from '@angular/core';
+import {  CommonModule } from '@angular/common';
+import {  MatToolbarModule } from '@angular/material/toolbar';
+import {  MatFormFieldModule  } from '@angular/material/form-field';
+import {  MatInputModule  } from '@angular/material/input';
+import {  MatIconModule } from '@angular/material/icon';
+import {  MatButtonModule } from '@angular/material/button';
 
 const MatModules = [
-  MatToolbarModule
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatIconModule,
+  MatButtonModule
 ]
 
 
